ea2cf649a0aa7861702f8f1496dce1a92cc93515
Pract16 <100338843+Pract16@users.noreply.github.com>
2022-03-03 10:27:42 +0300

    Update Whatisdoinguser.sql

--- Whatisdoinguser.sql
+++ Whatisdoinguser.sql
@@ -1,3 +1,6 @@
-create schema control;
-create table control.control(id serial,user_id int not null, id_note int, whatdoing varchar(128), date timestamp);
+
+--create schema control;
+create table control.control(id serial,user_id int not null, id_session int, whatdoing varchar(128), date timestamp);
+alter table control.control ADD CONSTRAINT fk_control_user FOREIGN KEY (user_id) references data.account(id);
+alter table control.control ADD CONSTRAINT fk_control_session FOREIGN KEY (id_session) references data.session(id);
 --USES Triggers
