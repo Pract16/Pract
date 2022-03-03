39a729d767af8b328fe388a37f1cf54f463555b8
Pract16 <100338843+Pract16@users.noreply.github.com>
2022-03-03 09:43:49 +0300

    Update createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -1,4 +1,4 @@
-create table account(
+create table data.account(
 id serial primary key,
 name varchar(128),
 first_name varchar(128),
@@ -7,7 +7,7 @@ pass varchar(64),
 status_onl int
 );
 
-create table session
+create table data.session
 (
 id serial primary key,
 id_account int not null,
@@ -16,27 +16,44 @@ time_begins timestamp,
 time_end timestamp
 );
 
-create table session_note
+create table data.session_note
 (session_id int not null,
 note_id int not null);
 
---(for another user)
---privelege 0-not readed
---1-can read
---2 can read and write
 
-create table note
+create table data.note
 (
 id serial primary key,
 account_id int not null,
-privelege int,
 text varchar(256),
 img bytea,
 date timestamp
 )
+--0 cant
+--1 can
+create table data.permission
+(id serial primary key,
+func varchar(128),
+per int);
+
+create table data.note_permission
+(
+id_user int,
+id_dependet_user id,
+permission varchar(64)
+);
+
+create table data.per_user(id_user int, id_per int);
 
 --add relations
-alter table session ADD CONSTRAINT fk_session_account FOREIGN KEY (id_account) references account(id);
-alter table session_note ADD CONSTRAINT fk_session_note FOREIGN KEY (note_id) references note(id);
-alter table session_note ADD CONSTRAINT fk_session_note2 FOREIGN KEY (session_id) references session(id);
-alter table note ADD CONSTRAINT fk_note_account FOREIGN KEY (account_id) references account(id);
+alter table data.session ADD CONSTRAINT fk_session_account FOREIGN KEY (id_account) references data.account(id);
+alter table data.session_note ADD CONSTRAINT fk_session_note FOREIGN KEY (note_id) references data.note(id);
+alter table data.session_note ADD CONSTRAINT fk_session_note2 FOREIGN KEY (session_id) references data.session(id);
+alter table data.note ADD CONSTRAINT fk_note_account FOREIGN KEY (account_id) references data.account(id);
+
+alter table data.per_user ADD CONSTRAINT fk_per_user FOREIGN KEY (id_per) references data.permission(id);
+alter table data.per_user ADD CONSTRAINT fk_pewr_user FOREIGN KEY (id_user) references data.users(id);
+
+alter table data.note_permission ADD CONSTRAINT fk_note_permission FOREIGN KEY (id_user) references data.account(id);
+alter table data.note_permission ADD CONSTRAINT fk_note_permission2 FOREIGN KEY (id_dependet_user) references data.account(id);
+
